23636732ae665eee8726714f00209a66e61f94ca
yandeu <20306025+yandeu@users.noreply.github.com>
2021-01-12 15:07:32 +0100

    Added cache

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -13,23 +13,48 @@ const fetchComments = (): Promise<string[]> => {
 }
 
 // the suspense component
-class Suspense extends Component<{ fallback: any; [key: string]: any }> {
+class Suspense extends Component<{ fallback: any; cache?: boolean; [key: string]: any }> {
   async didMount() {
-    // get promises
-    const { children, fallback, ...rest } = this.props
+    // get props promises in ...rest
+    const { children, fallback, cache = false, ...rest } = this.props
+
+    // strinify ...rest
+    const str = JSON.stringify(rest, function (_key, val) {
+      if (typeof val === 'function') return val + '' // implicitly `toString` it
+      return val
+    })
+
+    // create unique id based on ...rest
+    this.id = Nano.strToHash(JSON.stringify(str))
+
+    console.log('ID', this.id)
+
+    // set initial state to []
+    if (cache) this.initState = {}
+
+    // check if we already cached the results in this.state
+    if (cache && Object.keys(this.state).length > 0) {
+      this.addDataToChildren(this.state)
+      return
+    }
 
     // resolve the promises
-    const promises = Object.values(rest)
+    const promises = Object.values(rest).map((p: any) => p())
     const resolved = await Promise.all(promises)
 
     // prepare data
     const data = Object.keys(rest).reduce((obj, item, index) => {
+      if (cache) this.state = { ...this.state, [item]: resolved[index] }
       return {
         ...obj,
         [item]: resolved[index],
       }
     }, {})
 
+    this.addDataToChildren(data)
+  }
+
+  addDataToChildren(data: any) {
     // add data as props to children
     this.props.children.forEach((child: any) => {
       if (child.props) child.props = { ...child.props, ...data }
@@ -58,11 +83,18 @@ const Loading = () => <div>loading...</div>
 
 // App component
 class App extends Component {
+  didMount() {
+    setTimeout(() => {
+      this.update()
+    }, 2000)
+  }
+
   render() {
     return (
       <div>
         <h2>Comments</h2>
-        <Suspense comments={fetchComments()} fallback={<Loading />}>
+        {/* pass a function that resolves a promise */}
+        <Suspense cache comments={fetchComments} fallback={<Loading />}>
           <Comments />
         </Suspense>
       </div>
